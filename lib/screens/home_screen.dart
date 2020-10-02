c6723197d0dc3d543eb31d422df1b258c42db45f
Jeet1912 <51080094+Jeet1912@users.noreply.github.com>
2020-10-02 16:49:03 +0530

    Scrolling issue on home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:align_positioned/align_positioned.dart';
+import 'package:dystopia_flutter_app/widgets/pet_category.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
@@ -9,49 +9,6 @@ class HomePage2 extends StatefulWidget {
 
 class _HomePage2State extends State<HomePage2> {
   TextEditingController _search;
-
-  buildCarousel2(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.width + 40,
-      child: Column(
-        children: [
-          Stack(children: [
-            Container(
-              height: MediaQuery.of(context).size.width,
-              child: Swiper(
-                autoplay: true,
-                autoplayDelay: 3000,
-                duration: 1000,
-                curve: Curves.easeInOut,
-                autoplayDisableOnInteraction: true,
-                layout: SwiperLayout.STACK,
-                controller: SwiperController(),
-                itemWidth: MediaQuery.of(context).size.width,
-                itemCount: 3,
-                pagination: new SwiperPagination(),
-                control: new SwiperControl(
-                  size: 0.0,
-                ),
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                        bottomRight: Radius.circular(30)),
-                    child: Image(
-                      image: AssetImage('assets/images/Home_image.png'),
-                      filterQuality: FilterQuality.high,
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ]),
-        ],
-      ),
-    );
-  }
 
   Padding buildSearchBox() {
     return Padding(
@@ -72,7 +29,7 @@ class _HomePage2State extends State<HomePage2> {
               controller: _search,
               textAlign: TextAlign.right,
               decoration: InputDecoration(
-                  hintText: "Search for pets near you",
+                  hintText: "Search for pets...",
                   contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                   enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(
@@ -92,22 +49,223 @@ class _HomePage2State extends State<HomePage2> {
 
   @override
   Widget build(BuildContext context) {
-    /* 
-      SinglechildScrollView has been added to prevent RenderFlex overflowed error.
-      Error Source: Keyboard. 
-      */
-    return SingleChildScrollView(
-      child: Stack(
+    List<PetCategory> _tiles = [
+      PetCategory(
+        image: 'assets/images/dogs_icon.png',
+        name: "Dogs",
+      ),
+      PetCategory(
+        image: 'assets/images/cat.png',
+        name: "Cats",
+      ),
+      PetCategory(
+        image: 'assets/images/Other_icon.png',
+        name: "Others",
+      ),
+    ];
+    return Scaffold(
+      backgroundColor: Color(0xFFedf3eb),
+      body: Stack(
         children: [
-          buildCarousel2(context),
-          AlignPositioned.expand(
-            dx: 0,
-            dy: -23.5,
-            alignment: Alignment.bottomCenter,
-            child: buildSearchBox(),
-          )
+          CustomScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            slivers: <Widget>[
+              SliverAppBar(
+                collapsedHeight: MediaQuery.of(context).size.width / 1.25,
+                expandedHeight: (MediaQuery.of(context).size.width + 40) / 1.25,
+                backgroundColor: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                leadingWidth: 0.0,
+                leading: Opacity(
+                  opacity: 0.0,
+                ),
+                pinned: true,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Swiper(
+                    autoplay: true,
+                    autoplayDelay: 3000,
+                    duration: 1000,
+                    curve: Curves.easeInOut,
+                    autoplayDisableOnInteraction: true,
+                    layout: SwiperLayout.STACK,
+                    controller: SwiperController(),
+                    itemWidth: MediaQuery.of(context).size.width,
+                    itemCount: 3,
+                    pagination: new SwiperPagination(),
+                    control: new SwiperControl(
+                      size: 0.0,
+                    ),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        child: Image(
+                          image: AssetImage('assets/images/Home_image.png'),
+                          filterQuality: FilterQuality.high,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  color: Color(0xFFedf3eb),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 22,
+                    vertical: 30.0,
+                  ),
+                  height: MediaQuery.of(context).size.height / 4,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width / 4,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 5,
+                        ),
+                        child: _tiles[index],
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+          Positioned(
+            top: (MediaQuery.of(context).size.width + 35) / 1.25,
+            width: MediaQuery.of(context).size.width,
+            child: Align(
+              child: buildSearchBox(),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+/* TODO: Tried to implement Spotify scroll view. https://stackoverflow.com/questions/52133238/flutter-customscrollview-slivers-stackingx
+         Use innerScrollController to handle the offset of the searchButton. https://api.flutter.dev/flutter/widgets/NestedScrollViewState-class.html
+
+
+      ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = new ScrollController();
+    _scrollController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+// position of the search bar.
+  double get top {
+    double res = (MediaQuery.of(context).size.width + 35) / 1.25;
+    if (_scrollController.hasClients) {
+      double offset = _scrollController.offset;
+      if (offset < (res - MediaQuery.of(context).size.width / 1.25)) {
+        res -= offset;
+      } else {
+        res = MediaQuery.of(context).size.width / 1.25;
+      }
+    }
+    return res;
+  }
+
+          
+          NestedScrollView(
+            controller: _scrollController,
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return [
+ SliverAppBar(
+                collapsedHeight: MediaQuery.of(context).size.width / 1.25,
+                expandedHeight: (MediaQuery.of(context).size.width + 40) / 1.25,
+                backgroundColor: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                leadingWidth: 0.0,
+                leading: Opacity(
+                  opacity: 0.0,
+                ),
+                pinned: true,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 12,
+                        child: Container(),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: Text("Our stories"),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                    ],
+                  ),
+                  background: Swiper(
+                    autoplay: true,
+                    autoplayDelay: 3000,
+                    duration: 1000,
+                    curve: Curves.easeInOut,
+                    autoplayDisableOnInteraction: true,
+                    layout: SwiperLayout.STACK,
+                    controller: SwiperController(),
+                    itemWidth: MediaQuery.of(context).size.width,
+                    itemCount: 3,
+                    pagination: new SwiperPagination(),
+                    control: new SwiperControl(
+                      size: 0.0,
+                    ),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        child: Image(
+                          image: AssetImage('assets/images/Home_image.png'),
+                          filterQuality: FilterQuality.high,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              ];
+            },
+            body: ListView.builder() ,
+          ),*/
--- lib/widgets/pet_category.dart
+++ lib/widgets/pet_category.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PetCategory extends StatelessWidget {
-  final String emoji;
+  final String image;
   final String name;
   final Function pressButton;
   const PetCategory({
     Key key,
-    this.emoji,
+    this.image,
     this.name,
     this.pressButton,
   }) : super(key: key);
@@ -15,30 +15,45 @@ class PetCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
+      borderRadius: BorderRadius.circular(20),
       child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-        ),
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(10), boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 2),
+            blurRadius: 10,
+          )
+        ]),
         child: Material(
-          color: Colors.transparent,
           child: InkWell(
             onTap: pressButton,
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(emoji, style: TextStyle(fontSize: 50.0)),
-                  Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.nunito(
-                        textStyle: TextStyle(fontSize: 15.0)),
-                  )
-                ],
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFb9815d),
+                        image: DecorationImage(
+                          image: AssetImage(image),
+                          fit: BoxFit.cover,
+                        )),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text(
+                      name,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.nunito(
+                          textStyle: TextStyle(fontSize: 18.0)),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
